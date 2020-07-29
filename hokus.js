1ec4757ba147f2110c774b95d1db51c6b171368b
patrycja-io <patrycjatyra@protonmail.com>
2020-07-29 13:15:14 +0100

    added looping to find the value from 1 to  3

--- hokus.js
+++ hokus.js
@@ -42,12 +42,26 @@ const layout = [const layout = [
   // 3 - power-pellet
   // 4 - empty
 
+
+  const squares = []
+
+
   function createBoard() {
       for (let i=0; i < layout.length; i++){
           const square = document.createElement('div')
           grid.appendChild(square)
           square.push(square)
-
+          
+//add layout to the board
+      if(layout[i] === 0) {
+        squares[i].classList.add('pac-dot')
+      } else if (layout[i] === 1) {
+        squares[i].classList.add('wall')
+      } else if (layout[i] === 2) {
+        squares[i].classList.add('ghost-lair')
+      } else if (layout[i] === 3) {
+        squares[i].classList.add('power-pellet')
       }
-
+    }
   }
+  createBoard()
